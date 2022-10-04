2febeb9013af3a985a8d987e107eaf2a4840b763
2jun0 <soo28819@naver.com>
2022-10-04 17:44:09 +0900

    Update: 크롤러 사용 가이드 추가

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const DaumCrawler = require('../crawler/daum.crawler');
+const GoogleCrawler = require('../crawler/google.crawler');
+const NaverCrawler = require('../crawler/naver.crawler');
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    const daumCrawler = await DaumCrawler.getInstance();
+    const googleCrawler = await GoogleCrawler.getInstance();
+    const naverCrawler = await NaverCrawler.getInstance();
+});
 
 router.get('/', (req, res, next) => {
     res.render('home.html');
 });
 
 module.exports = router;
-
